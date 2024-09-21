f049389a3e7c5340bbcd98e03539367ab51e8b02
DimitriMeistermann <dimitri.mn@outlook.fr>
2024-09-21 09:02:11 +0300

    update basilisk env

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,10 +2,10 @@ pythonEnv <- BasiliskEnvironment(
     "env1",
     pkgname = "oob",
     channels = c("conda-forge", "bioconda"),
-    pip = c("trimap==1.0.15",
-        "igraph==0.11.6",
-                "leidenalg==0.10.2"),
+    pip = c("igraph==0.11.6",
+        "leidenalg==0.10.2",
+        "numpy==1.26.4"),
     packages = c(
-        "numpy==1.21.0"
+        "trimap==1.0.15"
     )
 )
